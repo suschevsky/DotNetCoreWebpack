25b4452878440fc9b439a2482c8fda802b1c0177
suschevsky <suschevsky.sv@gmail.com>
2017-04-24 00:30:28 +0500

    Add entry point for jqueryValidation

---
+++ src/jqueryValidation.ts
@@ -0,0 +1,7 @@
+//<reference path="jquery" />
+//import ddd from 'jquery-validation';
+//import * as $ from 'jquery';
+
+(() => {
+	console.log('%cTODO Import jquery-validation & jquery.validate.unobtrusive', 'font-size: 200%;color: red;');
+})();
