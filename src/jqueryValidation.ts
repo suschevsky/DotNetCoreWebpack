e6ef137fdccc915e247d66808552be6bb6eae236
suschevsky <suschevsky.sv@gmail.com>
2017-04-24 00:57:45 +0500

    jQuery import works fine

--- src/jqueryValidation.ts
+++ src/jqueryValidation.ts
@@ -1,7 +1,13 @@
-//<reference path="jquery" />
-//import ddd from 'jquery-validation';
-//import * as $ from 'jquery';
+import * as $ from 'jquery';
+
+//TODO Import validation somehow
+//import * as validate from 'jquery-validation';
+//import * as validationUnobtrusive 'jquery-validation-unobtrusive';
 
 (() => {
+	//Works fine:
+	//$('body').css('background-color', 'green');
+
 	console.log('%cTODO Import jquery-validation & jquery.validate.unobtrusive', 'font-size: 200%;color: red;');
+
 })();
